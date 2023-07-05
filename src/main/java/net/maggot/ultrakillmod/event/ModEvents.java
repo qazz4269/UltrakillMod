bfeaded902602509ffc24c3201dae0afcb9760d3
qazz4269 <memepolice07@gmail.com>
2023-07-04 21:54:27 -0400

    stuff

--- src/main/java/net/maggot/ultrakillmod/event/ModEvents.java
+++ src/main/java/net/maggot/ultrakillmod/event/ModEvents.java
@@ -3,7 +3,12 @@ package net.maggot.ultrakillmod.event;
 import net.maggot.ultrakillmod.UltrakillHell;
 import net.maggot.ultrakillmod.entity.ModEntities;
 import net.maggot.ultrakillmod.entity.custom.FilthEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -12,5 +17,11 @@ public class ModEvents {
     @SubscribeEvent
     public static void entityAttributeEvent(EntityAttributeCreationEvent event) {
         event.put(ModEntities.FILTH.get(), FilthEntity.setAttributes());
+    }
+
+    @SubscribeEvent
+    public static void entitySpawnRestrictions(SpawnPlacementRegisterEvent event) {
+        event.register(ModEntities.FILTH.get(), SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Monster::checkMonsterSpawnRules, SpawnPlacementRegisterEvent.Operation.REPLACE);
     }
 }
